87beb8c226047d60e686648aec946b570726aadb
EnricoBrandinelli <enrico.brandinelli.stud@ispascalcomandini.it>
2025-10-10 09:20:57 +0200

    ah

--- BlaisePascal.LessonExamples.Domain/Enemy.cs
+++ BlaisePascal.LessonExamples.Domain/Enemy.cs
@@ -23,25 +23,50 @@
 
         public Enemy(string name)
         {
-            Name = name;
+            SetName(name);
            
         }
 
 
         public Enemy(string name, int health)
         {
-           Name = name;
-            Health = health;
+            SetName(name);
+            SetHealth(health);
         }
 
+        private void SetHealth(int newHealth)
+        {
+            if (int.IsPositive(newHealth) && newHealth <= 100)
+            {
+                Health = newHealth;
+            }
+        }
+
+        private void SetName(string newName)
+        {
+            if (!string.IsNullOrWhiteSpace(newName))
+            {
+                Name = newName;
+            }
+
+        }
         private void TakeDamage(int damage)
         {
 
-            if (int.IsPositive(damage) && Health - damage > 0)
+            if (int.IsPositive(damage))
             {
+                //Health = Math.Max(0, Health - damage);
                 Health -= damage;
+                 
             }
-
+            if(Health <= 0)
+            {
+                isAlive = false;
+            }
+            else
+            {
+                isAlive = true;
+            }
         }
        
 
